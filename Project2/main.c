d41d626ede76234aedb0959ab537ba563e45fd07
Leon <Plong44555@gmail.com>
2026-02-21 18:58:34 +0100

    ddfpidbfver

--- Project2/main.c
+++ Project2/main.c
@@ -101,17 +101,19 @@ int main(int argc, char* argv[])
 		glVertex3f(-0.5f, 0.5f, 0.5f);
 		glVertex3f(-0.5f, 0.5f, -0.5f);
 
-		glColor3f(0.0f, 0.5f, 0.5f);
+		glColor3f(1.0f, 0.0f, 1.0f);
 		glVertex3f(0.5f, -0.5f, -0.5f);
 		glVertex3f(0.5f, 0.5f, -0.5f);
 		glVertex3f(0.5f, 0.5f, 0.5f);
 		glVertex3f(0.5f, -0.5f, 0.5f);
 
+		glColor3f(1.0f, 1.0f, 0.0f);
 		glVertex3f(-0.5f, 0.5f, -0.5f);
 		glVertex3f(-0.5f, 0.5f, 0.5f);
 		glVertex3f(0.5f, 0.5f, 0.5f);
 		glVertex3f(0.5f, 0.5f, -0.5f);
 
+		glColor3f(0.0f, 1.0f, 1.0f);
 		glVertex3f(-0.5f, -0.5f, -0.5f);
 		glVertex3f(0.5f, -0.5f, -0.5f);
 		glVertex3f(0.5f, -0.5f, 0.5f);
