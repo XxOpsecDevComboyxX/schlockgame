5e54661cb4c3634a7d979bb6cbc621ca312c3bbd
ttewrrr <ttewrrr@gmx.de>
2026-02-22 13:24:16 +0100

    Fixed and added calling Block Functions

--- Project2/Chunk.c
+++ Project2/Chunk.c
@@ -14,7 +14,6 @@ void renderChunk() {
 			for (height = 0; height < 10; height++) {
 				int index = width * 100 + length * 10 + height;
 				renderBlock(2.0f * width, 2.0f * height, 2.0f * length);
-				printf("| %d |", height);
 				blockPos[index].x = 2.0f * width;
 				blockPos[index].y = 2.0f * height;
 				blockPos[index].z = 2.0f * length;
